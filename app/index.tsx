3f7464cdfc59751a7fb9e715991c28dd26c1a3e8
Alyx DARENNE <axda.web@gmail.com>
2025-03-18 07:18:40 +0100

    feat: Working with Swift and XCode

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import Galaxies from "@/modules/galaxies";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>{Galaxies.hello()}</Text>
+      <Text>{Galaxies.PI}</Text>
+      <Text>{JSON.stringify(Galaxies.getDeviceInfo())}</Text>
+    </View>
+  );
+}

fcca6350977088fd669058093c52db9c4e6746d5
Alyx DARENNE <axda.web@gmail.com>
2025-03-20 08:31:57 +0100

    feat: Configure Push for IOS

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,18 @@
 import { Text, View } from "react-native";
 import Galaxies from "@/modules/galaxies";
+import { useState, useEffect } from "react";
+import { usePush } from "@/hooks/usePush";
 
 export default function Index() {
+  const { registerForPushNotificationsAsync } = usePush();
+  const [expoPushToken, setExpoPushToken] = useState("");
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      console.log(token);
+      setExpoPushToken(token);
+    });
+  }, []);
   return (
     <View
       style={{
@@ -12,7 +23,8 @@ export default function Index() {
     >
       <Text>{Galaxies.hello()}</Text>
       <Text>{Galaxies.PI}</Text>
-      {/* <Text>{JSON.stringify(Galaxies.getDeviceInfo())}</Text> */}
+      <Text>{JSON.stringify(Galaxies.getDeviceInfo())}</Text>
+      <Text>{expoPushToken}</Text>
     </View>
   );
 }

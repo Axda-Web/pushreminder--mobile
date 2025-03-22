7673173792f1ed9bf805dc39599defe9e50e268d
Alyx DARENNE <axda.web@gmail.com>
2025-03-22 08:35:10 +0100

    feat: Handling Notifications

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,59 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 import Galaxies from "@/modules/galaxies";
-import { useState, useEffect } from "react";
 import { usePush } from "@/hooks/usePush";
+import { useEffect, useState, useRef } from "react";
+import * as Notifications from "expo-notifications";
+import { Link, useRouter } from "expo-router";
 
 export default function Index() {
   const { registerForPushNotificationsAsync } = usePush();
   const [expoPushToken, setExpoPushToken] = useState("");
 
+  const notificationListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
+
+  const router = useRouter();
+
   useEffect(() => {
-    registerForPushNotificationsAsync().then((token) => {
-      console.log(token);
-      setExpoPushToken(token);
-    });
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        console.log("Push notification token:", token);
+        if (token) {
+          setExpoPushToken(token);
+        }
+      })
+      .catch((error) => {
+        console.error("Error registering for push notifications:", error);
+      });
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log("Notification received:", notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("Notification response received:", response);
+        router.push(`/${response.notification.request.content.data.pageid}`);
+      });
   }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ color: "yellow" }}>{Galaxies.hello()}</Text>
+    <View style={styles.container}>
+      <Text>{Galaxies.hello()}</Text>
       <Text>{Galaxies.PI}</Text>
-      {/* <Text>{JSON.stringify(Galaxies.getDeviceInfo())}</Text>*/}
+      <Text>{JSON.stringify(Galaxies.getDeviceInfo())}</Text>
       <Text>{expoPushToken}</Text>
+      <Link href="/42" asChild>
+        <Text>Go to page 42</Text>
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 10,
+  },
+});
